1c2a23084dc7380b4007d4074aab5b16af287e7b
RobertCharlton <robertcharlton23@outlook.com>
2018-09-10 11:50:04 +1000

    .

--- app/src/main/java/com/example/developer/messageservice/UserDataStorage.java
+++ app/src/main/java/com/example/developer/messageservice/UserDataStorage.java
@@ -12,11 +12,13 @@ import android.content.SharedPreferences;
 
 public class UserDataStorage {
 
-    private final SharedPreferences.Editor editor;
-    private final SharedPreferences preferences;
+    private final Context context;
+    private  SharedPreferences.Editor editor;
+    private  SharedPreferences preferences;
 
     private final static String SHARED_PREFERENCES_USERSPACE_KEY = "MyAppPreferences";
     private UserDataStorage(Context context) {
+        this.context = context;
         this.preferences = context.getSharedPreferences(SHARED_PREFERENCES_USERSPACE_KEY, 0);
         this.editor = preferences.edit();
     }
@@ -26,11 +28,17 @@ public class UserDataStorage {
     }
 
     public String getUserDataStringWithKey(String key) {
-        return preferences.getString(key, null);
+         preferences = context.getSharedPreferences(SHARED_PREFERENCES_USERSPACE_KEY, 0);
+         String string =  preferences.getString(key, null);
+         return string;
     }
 
     public void setUserDataStringForKey(String key, String userDataString) {
+        preferences = context.getSharedPreferences(SHARED_PREFERENCES_USERSPACE_KEY, 0);
+        this.editor = preferences.edit();
         editor.putString(key, userDataString);
-        editor.apply();
+        editor.commit();
+        preferences.getString(key, null);
+        if(true) {}
     }
 }
